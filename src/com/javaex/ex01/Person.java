09368b57b0c956d208ee4ad2e1eb30ff4a802335
wonho <wonho951@gmail.com>
2021-06-21 12:50:58 +0900

    2021.06.21 전화번호 관리 프로그램 만들기

---
+++ src/com/javaex/ex01/Person.java
@@ -0,0 +1,51 @@
+package com.javaex.ex01;
+
+public class Person {
+
+	//필드
+	private String name;
+	private String hp;
+	private String company;
+	
+	//생성자
+	public Person() {
+		super();
+	}
+	
+	public Person(String name, String hp, String company) {
+		super();
+		this.name = name;
+		this.hp = hp;
+		this.company = company;
+	}
+	
+	//메소드g/s
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	//메소드 - 일반
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", hp=" + hp + ", company=" + company + "]";
+	}
+	
+	
+	
+	
+}
---
+++ src/com/javaex/ex01/PersonApp.java
@@ -0,0 +1,10 @@
+package com.javaex.ex01;
+
+public class PersonApp {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
