4513ff8e05edde8564ebf8e78d490714eef0bf03
wonho <wonho951@gmail.com>
2021-06-21 15:46:19 +0900

    2021.06.21 전화번호 관리 프로그램 만들기

--- src/com/javaex/ex01/Person.java
+++ src/com/javaex/ex01/Person.java
@@ -1,49 +1,66 @@
 package com.javaex.ex01;
 
 public class Person {
-
-	//필드
+	//field
+	
 	private String name;
 	private String hp;
 	private String company;
 	
-	//생성자
-	public Person() {
-		super();
-	}
+	//constructor
 	
 	public Person(String name, String hp, String company) {
-		super();
+
 		this.name = name;
 		this.hp = hp;
-		this.company = company;
-	}
-	
-	//메소드g/s
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getHp() {
-		return hp;
-	}
-	public void setHp(String hp) {
-		this.hp = hp;
-	}
-	public String getCompany() {
-		return company;
-	}
-	public void setCompany(String company) {
 		this.company = company;
 	}
 
-	//메소드 - 일반
+	public Person() {
+		
+	}
+
+	//method g/s
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public void setNumber(int number) {
+	
+	}
+	
+	//method
+	
+	public void draw() {
+		System.out.println (name + "   " + hp + "     "+ company);
+		
+	}
 	@Override
 	public String toString() {
-		return "Person [name=" + name + ", hp=" + hp + ", company=" + company + "]";
+		return name + "   " + hp + "     "+ company;
 	}
+	
+	
+	
 	
 	
 	
--- src/com/javaex/ex01/PersonApp.java
+++ src/com/javaex/ex01/PersonApp.java
@@ -1,10 +1,78 @@
 package com.javaex.ex01;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class PersonApp {
+	public static void main(String[] args) throws Exception {
 
-	public static void main(String[] args) {
+		
+		Reader fr = new FileReader(".\\PhoneDB.txt");
+		BufferedReader br = new BufferedReader(fr);
+		
+		List<Person> pList = new ArrayList<Person>();
+		String line = "";
+		while (true) {
+			System.out.println("1.리스트" + "	" + "2.등록" + "	" + "3.삭제" + "	" + "4.검색" + "	" + "5.종료");
+			System.out.println("-----------------------------");
+			System.out.println(">메뉴선택: ");
+			line = br.readLine();
+			
+			if (line == null) {
+				break;
+			}
+			
+			String[] pInfo = line.split(",");
+			String name = pInfo[0];
+			String hp = pInfo[1];
+			String company = pInfo[2];
+			
+			Person person = new Person(name, hp, company);
+			pList.add(person);
+		}
+		
+
+		
+		System.out.println("*********************************");
+		System.out.println("*	전화번호 관리 프로그램		*");
+		System.out.println("*********************************");
+		
+		Scanner sc = new Scanner(System.in);
+		int select = sc.nextInt();
+		while (true) {
+			System.out.println("1.리스트" + "	" + "2.등록" + "	" + "3.삭제" + "	" + "4.검색" + "	" + "5.종료");
+			
+			if (select == 5) {
+				System.out.println("*********************************");
+				System.out.println("*	ㄳㄳㄳㄳㄳㄳㄳㄳ		*");
+				System.out.println("*********************************");
+				break;
+			}
+			
+			switch(select) {
+			case 1:
+				for(int i = 0; i < pList.size(); i++) {
+					System.out.println(i+1 + ".	" + pList.get(i).getName()+ "	" + pList.get(i).getHp()+ "	" + pList.get(i).getCompany());
+				}
+				break;
+			}
+			
+			sc.close();	
+		}
 		
 		
+		
+		
+	
+		/*
+		for(int i = 0; i < pList.size(); i++) {
+			System.out.println(i+1 + ".	" + pList.get(i).getName()+ "	" + pList.get(i).getHp()+ "	" + pList.get(i).getCompany());
+		}
+		*/
 	}
-
 }
